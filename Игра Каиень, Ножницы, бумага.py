48a81a7356706e99171ccf6faef0817ce8414823
clicedvly768 <153164423+clicedvly768@users.noreply.github.com>
2025-01-18 12:04:34 +0300

    Update Игра Каиень, Ножницы, бумага.py
    Изменёны файл, не тот код

--- Игра Каиень, Ножницы, бумага.py
+++ Игра Каиень, Ножницы, бумага.py
@@ -1,6 +1,13 @@
-def f (36,13)
-    if x == 24: return 0
-    if x<n: return 0
-    if x ==n: returm 1
-    return f(x-2,n) + f(x-3,n) + f(x//4,n)
-print(f(36,13))
+import random
+while True:
+    print('1-Stone')
+    print('2-Scissors')
+    print('3-Paper')
+    p = int(input())
+    n = random.randint(1,3)
+    if (p==n):
+            print("Draw")
+    elif ((p== 1 and n==2) or (p==1 and n==3) or (p==1 and n==1)):
+        print('Win')
+    else:
+        print('Won')
