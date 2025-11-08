ce87161f48d9a2ee826514547ffd90b12590902f
clicedvly768 <153164423+clicedvly768@users.noreply.github.com>
2025-11-08 11:25:37 +0300

    Create зкщп23.py

---
+++ зкщп23.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if 1<=a<=31:
+    print('Правильно')
+else:
+    print('Error')
+if 1<=b<=12:
+    print('Правильно')
+else:
+    print('Error')c == '666'c ==c == '666' '666'
+if 0<=c<=2038:
+    print('Правильно')
+else:
+    print('Error')
+
