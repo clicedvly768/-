9cb30dfd9ab51581760d35846b676e1212716b91
clicedvly768 <153164423+clicedvly768@users.noreply.github.com>
2025-11-22 12:13:24 +0300

    Обама

---
+++ s = int(input()).py
@@ -0,0 +1,22 @@
+def f(s,t,A):
+    if (s>10) or (t > A):
+        print('YES')
+    else:
+        Print('NO')
+
+    for A in range (-12,12):
+        Y = 0
+        N = 0
+        f(1,2,A)
+        f(11,2,A)
+        f(11,12,A)
+        f(1,12,A)
+        f(12,11,A)    
+        f(-1,-2,A)
+        f(-11,-12,A)
+        f(-12,-11,A)    
+        f(-12,11,A)
+        f(12,-11,A)
+        f(10,5,A)
+        f(10,10,A)   
+        print()
